ae86573a464b37077eff431948f14604f600a50f
rck <marcoslevis.rck@gmail.com>
2023-06-15 20:41:51 -0300

    anda

--- Aseguradora.UI/Program.cs
+++ Aseguradora.UI/Program.cs
@@ -1,18 +1,34 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-
-using Aseguradora.Repositorios;
-using Aseguradora.Aplicacion.UseCases;
-
 using Aseguradora.UI.Data;
 
 
+using Aseguradora.Repositorios;
+using Aseguradora.Aplicacion.UseCases;
+using Aseguradora.Aplicacion.Entidades;
+using Aseguradora.Aplicacion.Interfaces;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+var bd = new CrearBD();
+bd.Inicializar();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+
+// DICONTAINTER
+builder.Services.AddTransient<AgregarTitularUseCase>();
+builder.Services.AddScoped<IRepositorioTitular, RepositorioTitular>();
+
+
 
 var app = builder.Build();
 
@@ -21,14 +37,6 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
-
-
-builder.Services.AddTransient<AgregarTitularUseCase>();
-
-
-var bd = new CrearBD();
-bd.Inicializar();
-
 
 app.UseStaticFiles();
 
